ccfa783f2524f93df12f1dadbd4a32664b777c49
NullVoxPopuli <LPSego3+dev@gmail.com>
2019-11-28 00:03:47 -0500

    ooof. can't publish

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,10 +5,9 @@ set -e
 # This is temporary until I figure out a better name to publish
 # the package under, or I get rights to the package on npm
 
+yarn _prepublishOnly
+
 cat package.json | jq '.name = "ember-contextual-services--alpha"' | jq . > package.tmp.json
-
-# yarn _prepublishOnly
-
 mv package.json package.backup.json
 mv package.tmp.json package.json
 
