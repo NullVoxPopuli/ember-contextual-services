0d1ccf0b70cb9ae506738b874362da01323bdb68
NullVoxPopuli <LPSego3+dev@gmail.com>
2019-11-27 23:27:45 -0500

    finish publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
-cat package.json | jq '.name = "ember-contextual-services--alpha"' > package.json
 
-# cat package.json | jq '.name = ember-contextual-services'
+# This is temporary until I figure out a better name to publish
+# the package under, or I get rights to the package on npm
+
+cat package.json | jq '.name = "ember-contextual-services--alpha"' | jq . > package.tmp.json
+
+yarn _prepublishOnly
+
+mv package.json package.backup.json
+mv package.tmp.json package.json
+
+yarn publish
+
+mv package.backup.json package.json
+
+yarn _postpublish
+
+
+
