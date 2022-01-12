02389fc90d11649b541c1a1e028dfc020f8f5d9e
Camille TJHOA <camille.tjhoa@concordnow.com>
2022-01-12 16:52:37 +0000

    Fix examples according to the previous model fix
    Async issue fixed in d71c27a leads to other problems.
    
    First, the registry is based on the `routerService.currentRouteName` (cf [getContextInNearestRegistry](https://github.com/NullVoxPopuli/ember-contextual-services/blob/ade1071da41ceeb7127ecd5107aaafa5fb056729/addon/utils.ts#L24))
    
    As we now actively wait for the `model` hook to finish, the router transition is still pending.
    
    Unfortunately, Ember router service fields are not initialized 'til the current
    transition is not complete.
    
    So when Ember tries to resolve the contextual service injection in
    `model()` hook `getContextInNearestRegistry` fails as `currentRouteName`
    is `null`.
    
    To avoid this vivious cycle we must initialize contextual
    services **after** the transition is completed.
    
    Also note that we cannot use the
    [didTransition](https://api.emberjs.com/ember/4.1/classes/Route/events/didTransition?anchor=didTransition)
    as the router service is still not properly intialized at this
    stage (routerService.currentRouteName still `null`)

--- tests/dummy/app/routes/data-loading.js
+++ tests/dummy/app/routes/data-loading.js
@@ -1,4 +1,5 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 import {
   withContextualServices,
   context,
@@ -9,12 +10,21 @@ import { PersonService } from './-contexts/person-service';
 @withContextualServices(PersonService)
 export default class DataLoadingRoute extends Route {
   @context(PersonService) personService;
+  @service router;
+
+  constructor() {
+    super(...arguments);
+
+    this.router.on('routeDidChange', (transition) => {
+      if (transition.to.name === this.routeName) {
+        this.personService.data = this.currentModel;
+      }
+    });
+  }
 
   async model() {
     // await this.personService.nextPerson();
     let response = await fetch('https://swapi.dev/api/people/1/');
-    let json = await response.json();
-
-    this.personService.data = json;
+    return response.json();
   }
 }
